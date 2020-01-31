a0d9a88a8d6dd83aa122a95322e5cf50fef89045
a-shakra <shakra.anas@gmail.com>
2020-01-30 20:57:43 -0500

    bare bones dfs implemented

--- PuzzleGame/core.py
+++ PuzzleGame/core.py
@@ -1,51 +1,61 @@
 import numpy as np
+from helpers import build_initial_board, build_boards, Node, is_all_zeros
+
+
 def main():
+    with open('one_input') as input_file:
+        for line in input_file.readlines():
+            puzzle_dimension, max_depth, max_search_path, values = line.split()
+            root_board = np.array(build_initial_board(int(puzzle_dimension), values), np.int32)
+            root_node = Node(root_board, 0)
+            # start_dfs(root_node, max_depth)
+            build_boards(root_node)
+            for children in root_node.list_of_children:
+                print(children)
+                print("''''''''")
 
-    max_d = 7
-    board = np.array([[1, 1, 1], [0, 0, 1], [0, 1, 1]], np.int32)
-    print(board)
-    curr_x = 0
-    curr_y = 0
-    stack = [(curr_x, curr_y)]
-  
-    
-    visitedArray = np.zeros_like(board, dtype=bool)
 
-    while stack:
-        vertex = stack.pop()
-        visitedArray[vertex] = True
-        print(visitedArray)
-        flip(vertex[0], vertex[1], board)
-        print(board)
-        if(isGoldenState(board)):
-            print(board)
-        
+def start_dfs(node, max_depth):
+    visited = []
+    print(dfs(node, visited, int(max_depth)))
 
-def flip(xCoordinate, yCoordinate, array):
-    #flip requested cell
-    array[xCoordinate, yCoordinate] = 1 - array[xCoordinate, yCoordinate]
 
-    #flip horizontal neibours
-    if yCoordinate != 0:
-        array[xCoordinate, yCoordinate-1] = 1 - array[xCoordinate, yCoordinate-1]
-    if len(array[xCoordinate])-1 != yCoordinate:
-        array[xCoordinate, yCoordinate+1] = 1 - array[xCoordinate, yCoordinate+1]
+def dfs(node, visited_nodes, max_depth):
+    if node in visited_nodes:
+        return False
+    visited_nodes.append(node)
+    if is_all_zeros(node.game_board):
+        return True
+    if node.depth == max_depth:
+        return False
+    build_boards(node)
+    for child in node.list_of_children:
+        if dfs(child, visited_nodes, max_depth) is True:
+            return True
+    return False
+#     max_d = 7
+#     board = np.array([[1, 1, 1], [0, 0, 1], [0, 1, 1]], np.int32)
+#     print(board)
+#     curr_x = 0
+#     curr_y = 0
+#     stack = [(curr_x, curr_y)]
+#
+#
+#     visitedArray = np.zeros_like(board, dtype=bool)
+#
+#     while stack:
+#         vertex = stack.pop()
+#         visitedArray[vertex] = True
+#         print(visitedArray)
+#         flip(vertex[0], vertex[1], board)
+#         print(board)
+#         if(isGoldenState(board)):
+#             print(board)
 
-    #flip vertical neibours
-    if xCoordinate != 0:
-        array[xCoordinate-1, yCoordinate] = 1 - array[xCoordinate-1, yCoordinate]
-    if len(array[yCoordinate])-1 != xCoordinate:
-        array[xCoordinate+1, yCoordinate] = 1 - array[xCoordinate+1, yCoordinate]
 
-def isGoldenState(array):
-    return np.count_nonzero(array) == 0
+# def isGoldenState(array):
+#     return np.count_nonzero(array) == 0
 
 
 main()
-
-
-
-
-
-
 
--- PuzzleGame/helpers.py
+++ PuzzleGame/helpers.py
@@ -1,3 +1,81 @@
+import numpy as np
+import math
+
+
+# Class the represents a single node in the depth first search. Contains a parent game board plus all possible children
+# after one index has been flipped
+class Node:
+    def __init__(self, game_board, parent_depth):
+        self.game_board = game_board
+        self.list_of_children = []
+        self.depth = parent_depth + 1
+
+    def add_child(self, game_board):
+        self.list_of_children.append(game_board)
+
+
+# Function that receives concatenated board values from input file and returns a 2d dimensional array based on the
+# given dimensions of the board
+def build_initial_board(dimension, values):
+    split_values = [values[start:start+dimension] for start in range(0, len(values), dimension)]
+    board = []
+    # TODO Replace this with list comprehension or lambda expression, Compare performance after those changes made
+    for concatenated_row in split_values:
+        separated_row = []
+        for number in concatenated_row:
+            separated_row.append(int(number))
+        board.append(separated_row)
+    return board
+
+
+# Function that will build all the possible variations of a parent game board after exactly one index has been flipped
+# each board generated is then stored as a child to the parent node that contains the parent board
+def build_boards(parent_node):
+    parent_board = parent_node.game_board
+    for index, values in np.ndenumerate(parent_board):
+        child_board = parent_board.copy()  # .copy() to make an immutable copy as not to affect the parent board
+        flip(index[0], index[1], child_board)
+        child_node = Node(child_board, parent_node.depth)
+        parent_node.add_child(child_node)
+
+
+def find_best_board(parent_node):
+    best_child = None
+    smallest = math.inf
+    for child in parent_node.list_of_children:
+        first_zero_index = np.where(child == 0)[0][0]
+        if first_zero_index < smallest:
+            smallest = first_zero_index
+            best_child = child
+    return best_child
+
+
+def find_solution(board, max_depth):
+    pass
+
+
+def flip(xCoordinate, yCoordinate, array):
+
+    # flip requested cell
+    array[xCoordinate, yCoordinate] = 1 - array[xCoordinate, yCoordinate]
+
+    # flip horizontal neigbours
+    if yCoordinate != 0:
+        array[xCoordinate, yCoordinate-1] = 1 - array[xCoordinate, yCoordinate-1]
+    if len(array[xCoordinate])-1 != yCoordinate:
+        array[xCoordinate, yCoordinate+1] = 1 - array[xCoordinate, yCoordinate+1]
+
+    # flip vertical neigbours
+    if xCoordinate != 0:
+        array[xCoordinate-1, yCoordinate] = 1 - array[xCoordinate-1, yCoordinate]
+    if len(array[yCoordinate])-1 != xCoordinate:
+        array[xCoordinate+1, yCoordinate] = 1 - array[xCoordinate+1, yCoordinate]
+
+
+def is_all_zeros(array):
+    return np.count_nonzero(array) == 0
+
+
 def get_answer():
     """Get an answer."""
     return True
