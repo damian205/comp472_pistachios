3190930d223cde50f344e1001dfba4652ff87268
a-shakra <shakra.anas@gmail.com>
2020-02-04 15:40:26 -0500

    program now prints moves in chronological order

--- PuzzleGame/core.py
+++ PuzzleGame/core.py
@@ -1,6 +1,8 @@
 import numpy as np
 from helpers import build_initial_board, build_boards, Node, is_all_zeros
 
+list_of_moves = []  # Must be reset after every input puzzle
+
 
 def main():
     with open('one_input') as input_file:
@@ -10,13 +12,19 @@ def main():
             root_node = Node(root_board, 0)
             build_boards(root_node)
             start_dfs(root_node, max_depth)
+            list_of_moves.clear()
 
 
 def start_dfs(node, max_depth):
     visited = []
     if dfs(node, visited, int(max_depth)):
-        print(0, "  ", "".join(str(number) for number in node.game_board.flatten()))
-        print("''''''''")
+        starting_board_as_single_string = "".join(str(number) for number in node.game_board.flatten())
+        print(f'0  {starting_board_as_single_string}')
+        list_of_moves.reverse()
+        for board_move in list_of_moves:
+            print(board_move)
+    else:
+        print('No Solution')
 
 
 def dfs(node, visited_nodes, max_depth):
@@ -32,8 +40,8 @@ def dfs(node, visited_nodes, max_depth):
     for index, child in node.list_of_children.items():
         if dfs(child, visited_nodes, max_depth) is True:
             child_board_as_single_string = "".join(str(number) for number in child.game_board.flatten())
-            print(index, " ", child_board_as_single_string)
-            print("'''''''")
+            global list_of_moves
+            list_of_moves.append(f'{index} {child_board_as_single_string}')
             return True
     return False
 
