49d74e9fbcbb672f6dbe4a4a48e9e05a6f0413a6
a-shakra <a_shakra@live.concordia.ca>
2020-02-12 21:49:17 -0500

    basic structure of a star

--- PuzzleGame/core.py
+++ PuzzleGame/core.py
@@ -1,11 +1,16 @@
 import numpy as np
 from helpers import build_initial_board, build_boards, is_all_zeros, save_to_file
 from node import *
+import heapq
 
 list_of_solution_moves = []  # Must be reset after every input puzzle
 list_of_search_moves = []  # Ditto
 test_list_of_priorities = []
 list_of_nodes_a_star = []  # List used for the a star algorithm
+
+
+# TODO provide a copy() function for the Node class that will maintain all values but just clear the existing list of
+# children
 
 
 def main():
@@ -15,14 +20,13 @@ def main():
             puzzle_dimension, max_depth, max_search_path, values = line.split()
             root_board = np.array(build_initial_board(int(puzzle_dimension), values), np.int32)
             root_node = Node(root_board, 0, 0, 0)
-            start_dfs(root_node, max_depth)
-            save_to_file(list_of_solution_moves, f'{i}_dfs_solution.txt')
-            save_to_file(list_of_search_moves, f'{i}_dfs_search.txt')
+            start_dfs(root_node, max_depth, i)
+            root_node = Node(root_board, 0, 0, 0)  # reinitialize to remove preexisting list of children
+            start_a_star(root_node, max_search_path, i)
             i += 1
-            clean_up()
 
 
-def start_dfs(node, max_depth):
+def start_dfs(node, max_depth, i):
     node.__class__ = DfsNode
     build_boards(node, "DFS")
     visited = []
@@ -33,6 +37,9 @@ def start_dfs(node, max_depth):
         list_of_solution_moves.reverse()
     else:
         list_of_solution_moves.append('No Solution')
+    save_to_file(list_of_solution_moves, f'{i}_dfs_solution.txt')
+    save_to_file(list_of_search_moves, f'{i}_dfs_search.txt')
+    clean_up()
     sort_experiment(test_list_of_priorities)
 
 
@@ -65,23 +72,48 @@ def dfs(node, visited_nodes, max_depth):
     return False
 
 
-def start_a_star(node, max_search):
+def start_a_star(node, max_search, i):
     node.__class__ = AStarNode
-    visited = []
-    starting_board_as_single_string = "".join(str(number) for number in node.game_board.flatten())
-    list_of_search_moves.append(f'0  {starting_board_as_single_string}')
-    if dfs(node, visited, int(max_search)):
-        list_of_solution_moves.append(f'{0}  {starting_board_as_single_string}')
-        list_of_solution_moves.reverse()
-    else:
-        list_of_solution_moves.append('No Solution')
+    global list_of_nodes_a_star, list_of_solution_moves, list_of_search_moves
+    list_of_nodes_a_star.append(node)
+    a_star([], int(max_search))
+    list_of_solution_moves.reverse()
+    save_to_file(list_of_solution_moves, f'{i}_A_solution.txt')
+    save_to_file(list_of_search_moves, f'{i}_A_search.txt')
+    clean_up()
     sort_experiment(test_list_of_priorities)
+
+
+def a_star(visited_nodes, max_search):
+    global list_of_nodes_a_star, list_of_search_moves, list_of_solution_moves
+    if len(list_of_nodes_a_star) == 0:
+        return False
+    if len(list_of_search_moves) >= max_search:
+        return False
+    current_best_node = list_of_nodes_a_star.pop(0)
+    board_as_single_string = "".join(str(number) for number in current_best_node.game_board.flatten())
+    list_of_search_moves.append(f'{current_best_node.index} {board_as_single_string}')
+    # check to see if board of the same values has been visited before
+    if len(set([board_as_single_string]).intersection(set(visited_nodes))) != 0:
+        return False
+    visited_nodes.append(board_as_single_string)
+    if is_all_zeros(current_best_node.game_board):
+        list_of_solution_moves.append(f'{current_best_node.index} {board_as_single_string}')
+        return True
+    build_boards(current_best_node, "A*")
+    for child in current_best_node.list_of_children:
+        list_of_nodes_a_star.append(child)
+    list_of_nodes_a_star.sort()
+    if a_star(visited_nodes, max_search):
+        list_of_solution_moves.append(f'{current_best_node.index} {board_as_single_string}')
+        return True
 
 
 def clean_up():
     list_of_solution_moves.clear()
     list_of_search_moves.clear()
     test_list_of_priorities.clear()
+    list_of_nodes_a_star.clear()
 
 
 def sort_experiment(list_of_stuff):
